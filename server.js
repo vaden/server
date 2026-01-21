58eef15c0ad40282fee3f36485655075c140f62e
QuackRanger <valadonmaxime@gmail.com>
2026-01-21 13:26:41 +0100

    ajout env

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const mongoose = require("mongoose");
-const app = require("./app");
-
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./app');
 const port = process.env.PORT || 4000;
-const mongoUri =
-  "mongodb+srv://MaximeV:L2u78M9wAXCfphe@cluster0.entkqfa.mongodb.net/grimoire?retryWrites=true&w=majority";
+const mongoUri = process.env.MONGODB;
 
 mongoose
   .connect(mongoUri)
   .then(() => {
-    console.log("Connexion à MongoDB réussie !");
+    console.log('Connexion à MongoDB réussie !');
 
     app.listen(port, () => {
       console.log(`Serveur démarré sur le port ${port}`);
     });
   })
   .catch((err) => {
-    console.error("Erreur de connexion MongoDB:", err.message);
+    console.error('Erreur de connexion MongoDB:', err.message);
     process.exit(1);
   });
